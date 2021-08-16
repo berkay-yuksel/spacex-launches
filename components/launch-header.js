59122c213252a8afb29be25cb625f3a0027fc869
berkay-yuksel <98.berkay.yuksel@gmail.com>
2021-08-16 06:21:37 +0300

    adding launch header with styles

---
+++ components/launch-header.js
@@ -0,0 +1,38 @@
+import styles from "../styles/launch-header.module.css";
+import logo from "../public/logo.png"
+import Link from "next/link";
+import Image from 'next/image'
+const LaunchHeader=({next})=> {
+    return (
+        <div className={styles.launch_header_container}>
+<div className={styles.logo}>
+<Link href='/' >
+<Image src={logo} alt="logo" />
+</Link>
+</div>
+<div className={styles.info_box}>
+<span>NEXT LAUNCH</span>
+<table>
+  <tr>
+    <th>Flight Number</th>
+    <th>Mission</th>
+    <th>Rocket Number</th>
+    <th>Launch Date</th>
+  </tr>
+  <tr>
+    <td>{next.flight_number}</td>
+    <td> {next.mission_name}</td>
+    <td>{next.rocket.rocket_name}</td>
+     <td>{next.launch_date_local.split("T")[0]}</td>
+  </tr>
+</table>
+</div>
+
+        </div>
+    )
+}
+
+export default LaunchHeader
+
+
+// <br/> <br/>  <br/> 
---
+++ components/launch-list.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+import errorImage from "../public/undefinedholder.jpg";
+import styles from "../styles/launch-list.module.css";
+
+const LaunchList = ({ past }) => {
+  return (
+    <div className={styles.launch_list_container}>
+      {past
+        .slice(0)
+        .reverse()
+        .map((item) => (
+          <Link href={`/launch/${item.flight_number}`}>
+            <div className={styles.launch_item} key={item.flight_number}>
+              <Image
+                height="270px"
+                width="330px"
+                src={
+                  `${item.links.flickr_images[0]}` === "undefined"
+                    ? errorImage
+                    : `${item.links.flickr_images[0]}`
+                }
+              />
+              <p>{item.flight_number}</p>
+            </div>
+          </Link>
+        ))}
+    </div>
+  );
+};
+
+export default LaunchList;
